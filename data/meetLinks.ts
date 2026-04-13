d582ed9c5ef60ad0b37fc498a64e84fbb6301ec6
Arya Patel <97847987+Arya-P05@users.noreply.github.com>
2026-04-13 10:27:30 -0400

    static const files

---
+++ data/meetLinks.ts
@@ -0,0 +1,13 @@
+export const MEET_LINKS: { [key: string]: string } = {
+  "Andrew Martinko|2025-07-28|10:00": "https://meet.google.com/hea-vcji-tcz",
+  "Irene (Heejin) Koo|2025-07-28|15:00": "https://meet.google.com/ffu-zmvf-iws",
+  "Irene (Heejin) Koo|2025-07-28|16:00": "https://meet.google.com/ffu-zmvf-iws",
+  "Andrew Martinko|2025-07-29|10:00": "https://meet.google.com/yey-fwud-ahn",
+  "Jimmy Yun|2025-07-29|14:00": "https://meet.google.com/dcx-yrey-qxw",
+  "Jimmy Yun|2025-07-30|14:00": "https://meet.google.com/oct-gexp-niu",
+  "Krysta Traianovski|2025-07-31|16:00": "https://meet.google.com/gwp-zdfj-buh",
+  "Mischa Hamara|2025-08-01|10:00": "https://meet.google.com/pfw-xyfg-ghx",
+  "Mischa Hamara|2025-08-01|11:00": "https://meet.google.com/pfw-xyfg-ghx",
+  "Krysta Traianovski|2025-08-01|15:00": "https://meet.google.com/tws-yzva-upr",
+  "Irene (Heejin) Koo|2025-08-01|16:00": "https://meet.google.com/ffu-zmvf-iws",
+};
